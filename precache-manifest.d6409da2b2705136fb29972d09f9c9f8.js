eddb3a5d2534f87dce563f8e5f0b89f2ec512851
SrinivasI <ippilisrinivas715@gmail.com>
2020-06-30 08:21:25 +0530

    Updates

---
+++ precache-manifest.d6409da2b2705136fb29972d09f9c9f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2618dc86d0020c900d196d61ffc8e012",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9554b8b66a8308124007",
+    "url": "/react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "65ec7679a28fe1be6aae",
+    "url": "/react-app/static/js/2.57e19943.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.57e19943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9554b8b66a8308124007",
+    "url": "/react-app/static/js/main.acb54bb4.chunk.js"
+  },
+  {
+    "revision": "8b1aae3a6bbbe9e49666",
+    "url": "/react-app/static/js/runtime-main.064bafa3.js"
+  }
+]);
